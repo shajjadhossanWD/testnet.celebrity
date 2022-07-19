2f6664431f5bf0b7ab1630f4aec80033f8a6bd4c
pravigya <pravigyajain5@gmail.com>
2022-07-19 22:14:38 +0530

    test

---
+++ src/utils/constant.js
@@ -0,0 +1,14 @@
+import mint from "./MintNFT.json";
+import token from "./BEP20.json";
+import usdsc from "./USDSC.json";
+import dsltestnet from "./DSLTestnet.json";
+
+export const mintAddressTestnet = "0x4bB966e2ECf931D4cb9DCEd2216070daE1cbcDeE";
+export const mintABITestnet = mint.abi;
+export const USDSCtokenAddressTestnet =
+  "0x474DeB3F462A5A86B885AadB4Ed22D5bBA93eAbb";
+export const USDSCtokenABITestnet = token.abi;
+
+export const DSLtokenAddressTestnet =
+  "0xb1eCd4C8C6EEF40EF4EB6a066821C7b3227083d5";
+export const DSLtokenABITestnet = dsltestnet.abi;
