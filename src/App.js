a8378920135abcb5dfe2b2330d714a8d3984be48
MD Sanju <mdsanju387@gmail.com>
2022-07-20 18:52:35 +0600

    Edit NFT Page Created

--- src/App.js
+++ src/App.js
@@ -9,21 +9,22 @@ import Publiclayout from './components/layout/Publiclayout';
 import Dashboard from './components/page/Dashboard/Dashboard';
 import DashboardAdmin from './components/page/Dashboard/DashboardAdmin';
 import DashboardNfts from './components/page/Dashboard/DashboardNfts';
-import Home from './components/page/Home/Home';
-import India from './components/page/India';
-import Malaysia from './components/page/Malaysia';
-import MealDetails from './components/page/MealNft/Details.Meal';
-import MealNFT from './components/page/MealNft/MealNFT';
-import Singapore from './components/page/Singapore';
-import SouvenirDetails from './components/page/Souvenir/Details.Souvenir';
-import SouvenirNFT from './components/page/Souvenir/SouvenirNFT';
-import ScrollToTop from './components/ScrollToTop';
+import EditNft from "./components/page/Dashboard/EditNft";
+import Home from "./components/page/Home/Home";
+import India from "./components/page/India";
+import Malaysia from "./components/page/Malaysia";
+import MealDetails from "./components/page/MealNft/Details.Meal";
+import MealNFT from "./components/page/MealNft/MealNFT";
+import Singapore from "./components/page/Singapore";
+import SouvenirDetails from "./components/page/Souvenir/Details.Souvenir";
+import SouvenirNFT from "./components/page/Souvenir/SouvenirNFT";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="dark-scheme de-clivus">
       <div id="wrapper">
-        <ScrollToTop/>      
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Publiclayout></Publiclayout>}>
             <Route path="/" element={<Landing />} />
@@ -33,13 +34,23 @@ function App() {
             <Route path="/malaysia" element={<Malaysia></Malaysia>} />
             <Route path="/singapore" element={<Singapore></Singapore>} />
             <Route path="/souvenirnft" element={<SouvenirNFT></SouvenirNFT>} />
-            <Route path="/souvenirnft/:souvenirId" element={<SouvenirDetails/>} />
+            <Route
+              path="/souvenirnft/:souvenirId"
+              element={<SouvenirDetails />}
+            />
             <Route path="/mealnft" element={<MealNFT></MealNFT>} />
-            <Route path="/mealnft/:mealnId" element={<MealDetails/>} />
+            <Route path="/mealnft/:mealnId" element={<MealDetails />} />
           </Route>
           <Route path="/dashboard" element={<Dashboard></Dashboard>}>
             <Route index element={<DashboardAdmin></DashboardAdmin>} />
-            <Route path="/dashboard/dnfts" element={<DashboardNfts></DashboardNfts>} />
+            <Route
+              path="/dashboard/dnfts"
+              element={<DashboardNfts></DashboardNfts>}
+            />
+            <Route
+              path="/dashboard/dnfts/editNft"
+              element={<EditNft></EditNft>}
+            />
           </Route>
           <Route path="*" element={<NotFound></NotFound>} />
         </Routes>
