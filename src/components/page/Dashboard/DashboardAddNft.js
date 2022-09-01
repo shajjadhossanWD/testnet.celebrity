f2d57d732eef21cd9e51440138fbd524c560db1a
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-09-01 22:24:13 +0600

    c

--- src/components/page/Dashboard/DashboardAddNft.js
+++ src/components/page/Dashboard/DashboardAddNft.js
@@ -138,7 +138,7 @@ const DashboardAddNft = () => {
         let output = document.getElementById('output');
         output.src = URL.createObjectURL(event.target.files[0]);
         output.onload = function () {
-            URL.revokeObjectURL(output.src) // free memory
+            URL.revokeObjectURL(output.src)
         }
     };
 
